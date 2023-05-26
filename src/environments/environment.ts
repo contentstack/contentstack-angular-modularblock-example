6ee3707d01164695437a6e96a3179ce06b585053
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2023-05-26 13:07:41 +0530

    fix:ECO-2999 LICENSE updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
-
-  export const environment = {
-    production: false,
-    config : {
-      api_key: 'blta7d00db72b5f1572',
-      delivery_token: 'csaa3f1929c929904191d448ea',
-      environment: 'preview',
-      region: 'us'
-    }
-  };
+export const environment = {
+  production: false,
+  config: {
+    api_key: '',
+    delivery_token: '',
+    environment: '',
+    region: ''
+  }
+};
