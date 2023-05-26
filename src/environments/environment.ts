37bdb17b3f29b010850c38abe5e0eb77f3c74697
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2023-05-26 12:59:21 +0530

    fix:env file generation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
-export const environment = {
-  production: false,
-  config: {
-    api_key: '',
-    delivery_token: '',
-    environment: '',
-    region: ''
-  }
-};
+
+  export const environment = {
+    production: false,
+    config : {
+      api_key: 'blta7d00db72b5f1572',
+      delivery_token: 'csaa3f1929c929904191d448ea',
+      environment: 'preview',
+      region: 'us'
+    }
+  };
